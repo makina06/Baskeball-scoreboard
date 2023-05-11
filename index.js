0d69367acb322f131e87d40ec5916439509f8318
Max <119096559+makina06@users.noreply.github.com>
2023-05-11 19:18:12 +1200

    add functionality to uptade score check winner. integrate checkWinner function to update score function

--- index.js
+++ index.js
@@ -7,29 +7,35 @@ let resultScoreGuest = document.getElementById("result-point-guest");
 function addOne() {
   scoreHome += 1;
   resultScoreHome.textContent = scoreHome;
+  checkWinner();
 }
 
 function addTwo() {
   scoreHome += 2;
   resultScoreHome.textContent = scoreHome;
+  checkWinner();
 }
 function addThree() {
   scoreHome += 3;
   resultScoreHome.textContent = scoreHome;
+  checkWinner();
 }
 
 function addOneGuest() {
   scoreGuest += 1;
   resultScoreGuest.textContent = scoreGuest;
+  checkWinner();
 }
 function addTwoGuest() {
   scoreGuest += 2;
   resultScoreGuest.textContent = scoreGuest;
+  checkWinner();
 }
 
 function addThreeGuest() {
   scoreGuest += 3;
   resultScoreGuest.textContent = scoreGuest;
+  checkWinner();
 }
 
 function newGame() {
@@ -37,4 +43,20 @@ function newGame() {
   scoreGuest -= scoreGuest;
   resultScoreHome.textContent = 0;
   scoreHome -= scoreHome;
+}
+
+function checkWinner() {
+  if (scoreHome < scoreGuest) {
+    resultScoreGuest.style.color = "yellow";
+    resultScoreHome.style.color = "#f94f6d";
+  } else if (scoreHome > scoreGuest) {
+    resultScoreGuest.style.color = "#f94f6d";
+    resultScoreHome.style.color = "yellow";
+  } else if (scoreHome === scoreGuest) {
+    resultScoreGuest.style.color = "green";
+    resultScoreHome.style.color = "green";
+  } else {
+    resultScoreGuest.style.color = "#f94f6d";
+    resultScoreHome.style.color = "#f94f6d";
+  }
 }
